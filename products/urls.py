7e4d4f853b7e36964c5a4e69459e052e34e937d9
Ranburu <slymihome@gmail.com>
2020-02-05 01:12:44 +0300

    Edit links

--- products/urls.py
+++ products/urls.py
@@ -3,8 +3,8 @@ from .views import product_create_view, dynamic_lookup_view, product_delete_view
 
 app_name = 'products'
 urlpatterns = [
-    path('create/', product_create_view),
+    path('create/', product_create_view, name="product-create"),
     path('<int:my_id>/delete', product_delete_view),
     path('<int:my_id>/', dynamic_lookup_view, name='product-detail'),
-    path('', product_list_view),
+    path('', product_list_view, name="product-list"),
 ]
--- tryDjango/urls.py
+++ tryDjango/urls.py
@@ -16,12 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from pages.views import home_view, contact_view
-from products.views import product_create_view, render_initial_data, dynamic_lookup_view, product_delete_view, product_list_view
 
 
 urlpatterns = [
     path('products/', include('products.urls')),
-    path('', home_view),
+    path('', home_view, name="home"),
     path('contact/', contact_view),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name="login"),
 ]
